e44b95958c1308aab78e049c5af55d1f7869702d
Tatsuya1206 <49546607+Tatsuya1206@users.noreply.github.com>
2023-02-02 19:25:04 +0900

    Add files via upload

--- ChromeAddOn/ProjctWorkinghours/ProjctWorkinghours.js
+++ ChromeAddOn/ProjctWorkinghours/ProjctWorkinghours.js
@@ -1,37 +1,36 @@
-//合計行の始まりと、終わりの行数を取得する。
-//合計行の表示、非表示を実装する。
-
 const PROJ_COL_NAME = "プロジェクト";
 const WORKING_HOURS_COL_NAME = "実働";
 const SEL_LIST_DEFAULT_ITEM = "選択してください。";
 const PROJ_LIST_ID = "projList";
-
-//let で合計行の始まりと、終わりの行数取得
+const TOTAL_ROWS_COLSPAN = 5;
 
 window.onload = function(){
     // 対象テーブルを取得
     var myTable = document.getElementsByTagName('table')[0];
     // ドロップダウンリスト作成
     createProjDropDownList(myTable);
-    //ここで、合計行の始まりを取得
-    //総合計行インデックス取得
-    // var grandTotalRowIndex = myTable.rows.length;
+
+    //合計行の最初の行数を取得
+    var totalStartRowIndex = myTable.rows.length + 1;
+    setHiddenItem('totalStartRowIndex',totalStartRowIndex);
 
     var listItemArr = getListItem(myTable,PROJ_COL_NAME);
-    for(var i = 2;i < listItemArr.length;i++){
+    for(var i = 1;i < listItemArr.length;i++){
         // プロジェクト別合計行を追加
         addTotalRow(myTable,listItemArr[i],i);
     }
-    //ここで、合計行の最後の行数を取得
+    //合計行の最後の行数を取得
+    var totalEndRowIndex = myTable.rows.length;
+    setHiddenItem('totalEndRowIndex',totalEndRowIndex);
 }
 
+// ドロップダウン作成
 function createProjDropDownList(table){
     // ・ドロップダウン用の値の取得
     var listItemArr = getListItem(table,PROJ_COL_NAME);
 
     // ・ドロップダウンコントロールの生成
     createDropDown(PROJ_LIST_ID,listItemArr,table);
-
 }
 
 // リストアイテム取得　引数：テーブル、リスト化したい対象のカラム名
@@ -46,7 +45,7 @@ function getListItem(table,targetListItemName){
     listItemArr.push(SEL_LIST_DEFAULT_ITEM);
     // ドロップダウンリストの項目を取得
     for (var i = 2; i < table.rows.length-1; i++) {
-
+        
         // テーブルからドロップダウンリストの値を取得
         itemName = table.rows[i].cells[targetListItemIndex].innerHTML;
 
@@ -55,7 +54,6 @@ function getListItem(table,targetListItemName){
             listItemArr.push(itemName);
         }
     }
-
     // ドロップダウンリストの値を返す
     return listItemArr;
 }
@@ -82,73 +80,79 @@ function createDropDown(itemId,ItemArr,table){
     selectList.addEventListener('change', dropDownChanged);
     
     for(i = 0;i < ItemArr.length;i++){
-
-        //ドロップダウンのText,Valueを設定
         var selItem = document.createElement('option');
         selItem.text=ItemArr[i];
         selItem.value=i;
         selectList.appendChild(selItem);
     }
-
     td.appendChild(selectList);
     tr.appendChild(td);
-
 }
 
+// ドロップダウンインデックスチェンジ
 function dropDownChanged(e) {
     var selectList = document.getElementById(PROJ_LIST_ID);
+    var totalStartRowIndex = Number(document.getElementById("totalStartRowIndex").value);
+    var totalEndRowIndex = Number(document.getElementById("totalEndRowIndex").value);
     let index = Number(e.target.value);
     let text = selectList.options[index].text
     var table = document.getElementsByTagName("table")[0];
-    displayRowChange(table,text);
+    displayRowChange(table,text,totalStartRowIndex);
+    displayTotalRowChange(table,text,totalStartRowIndex,totalEndRowIndex)
 }
 
-
-function displayRowChange(table,selectProjName){
-    // 今は総合計行も表示、非表示が切り替わる。
-
-    // プロジェクト名のカラム位置取得
+// テーブル行の表示非表示制御
+function displayRowChange(table,selectProjName,totalStartRowIndex){
     var projNameIndex = getTargetColIndex(table,PROJ_COL_NAME);
 
-    // テーブル要素のrowでループ
-    for(var i = 2;i < table.rows.length;i++){
-
-        // 選択されたプロジェクトの場合
+    for(var i = 2;i < totalStartRowIndex - 2;i++){
         if(selectProjName === table.rows[i].cells[projNameIndex].innerHTML){
-
-            // 行を表示。
-            table.rows[i].style.display = "table-row";
-
+            changeShowHide(table.rows[i],true);
         }else if(selectProjName === SEL_LIST_DEFAULT_ITEM){
-            // 行を表示。
-            table.rows[i].style.display = "table-row";
+            changeShowHide(table.rows[i],true);
         }else{
-
-            // 上記以外の場合
-            // 行を非表示。
-            table.rows[i].style.display = "none";
+            changeShowHide(table.rows[i],false);
         }
     }
 }
 
+// テーブルの合計行の表示非表示制御
+function displayTotalRowChange(table,selectProjName,totalStartRowIndex,totalEndRowIndex){
+    var projNameIndex = getTargetColIndex(table,PROJ_COL_NAME);
+
+    for(var i = totalStartRowIndex - 1; i < totalEndRowIndex;i++){
+        if(selectProjName === table.rows[i].cells[(projNameIndex - TOTAL_ROWS_COLSPAN) + 1].innerHTML){
+            changeShowHide(table.rows[i],true);
+        }else if(selectProjName === SEL_LIST_DEFAULT_ITEM){
+            changeShowHide(table.rows[i],true);
+        }else{
+            changeShowHide(table.rows[i],false);
+        }
+    }
+}
+
+// テーブルの行の非表示/非表示
+function changeShowHide(tr,isShow){
+    if(isShow){
+        tr.style.display = "table-row";
+    }else{
+        tr.style.display = "none";
+    }
+}
 
 // 合計行追加
 function addTotalRow(table,targetProjName,totalRowIndex){
-    
     var totalWorkingHours = 0;
     var tr = document.createElement("tr");
-    // tr.setAttribute("id","TotalRow" + i );
     totalWorkingHours = getTotalWorkinghours(table,WORKING_HOURS_COL_NAME,targetProjName,totalRowIndex);
-    // 取得した合計時間を合計行に設定
     createTotalRow(tr,totalWorkingHours,targetProjName);
     table.appendChild(tr);
 }
 
 // 合計行作成
 function createTotalRow(tr,totalWorkingHours,targetProjName){
-
     var td = document.createElement("td");
-    td.setAttribute("colspan","5");
+    td.setAttribute("colspan",String(TOTAL_ROWS_COLSPAN));
     tr.appendChild(td);
 
     // 作業時間表示列
@@ -163,26 +167,24 @@ function createTotalRow(tr,totalWorkingHours,targetProjName){
     td3.innerHTML = targetProjName
     tr.appendChild(td3);
 
+    //td調整
     var td4 = document.createElement("td");
     tr.appendChild(td4);
     var td5 = document.createElement("td");
     tr.appendChild(td5);
 }
 
-// 合計時間取得　引数：テーブル、集計対象項目名、対象プロジェクト名
+// 合計時間取得
 function getTotalWorkinghours(table,sumTargetColName,targetProjName,totalRowIndex){
 
     var sumTargetColIndex = 0;
     var sumTargetKeyIndex = 0;
     sumTargetColIndex = getTargetColIndex(table,sumTargetColName);
     sumTargetKeyIndex = getTargetColIndex(table,PROJ_COL_NAME);
-
     var tmpHours = 0;
     var tmpMinuets = 0;
-
     var hours = "";
     var minuets = "";
-
     var tmpFullSecond = 0;
 
     for (var n=2; n < table.rows.length - totalRowIndex; n++) {
@@ -207,31 +209,19 @@ function getTotalWorkinghours(table,sumTargetColName,targetProjName,totalRowInde
     return hours + ':' + minuets;
 }
 
+// 時間表示フォーマット
 function timeFormat(arg){
     if(arg.length < 2){
         arg = "0" + arg;
-        
     }
     return arg;
 }
 
-
-// // 隠し項目設定　引数：Name属性に設定したい文字列、そのNameに属する、値（複数可）
-// function setHiddenItem(itemName,ItemArr){
-
-//     var bodyElement = document.getElementsByTagName('body');
-
-//     for(i = 0;i < ItemArr.length;i++){
-//         var hiddenItem = document.createElement("input");
-        
-//         hiddenItem.setAttribute("type","hidden");
-//         hiddenItem.setAttribute("name",itemName);
-//         hiddenItem.setAttribute("value",ItemArr[i]);
-
-//         bodyElement.appendChild(hiddenItem);
-
-//     }
-// }
-
-
-
+// 隠し項目設定
+function setHiddenItem(id,value){
+    var hiddenItem = document.createElement("input");
+    hiddenItem.setAttribute("type","hidden");
+    hiddenItem.setAttribute("id",id);
+    hiddenItem.setAttribute("value",value);
+    document.body.appendChild(hiddenItem);
+}
